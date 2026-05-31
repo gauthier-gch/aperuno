1ff2f3ed06f677d6fbf343991b660220cdd9148d
Gaugau <gauthier.gache@gmail.com>
2026-05-31 20:53:56 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// base: "./" -> les chemins d'assets sont relatifs, ce qui marche aussi bien
-// sur Vercel (racine) que sur GitHub Pages (sous-dossier /nom-du-repo/).
-export default defineConfig({
-  base: "./",
-  plugins: [react()],
-});
